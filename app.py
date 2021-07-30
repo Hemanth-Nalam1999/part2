6b1097988e2aa3a97ed199fbb992397a088d8bac
Hemanth-Nalam <nalamhemanth1999@.com>
2021-07-30 21:57:08 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # doing necessary imports
 
 from flask import Flask, render_template, request,jsonify
-# from flask_cors import CORS,cross_origin
+from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
@@ -9,39 +9,44 @@ from urllib.request import urlopen as uReq
 app = Flask(__name__)  # initialising the flask app with the name 'app'
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def homepage():
     return render_template('index.html')
 
 # base url + /
 #http://localhost:8000 + /
 @app.route('/scrap',methods=['POST']) # route with allowed methods as POST and GET
+@cross_origin()
 def index():
     if request.method == 'POST':
-        searchString = request.form['content'].replace(" ","") # obtaining the search string entered in the form
+
         try:
+            searchString = request.form['content'].replace(" ", "")  # obtaining the search string entered in the form
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString # preparing the URL to search the product on flipkart
             uClient = uReq(flipkart_url) # requesting the webpage from the internet
             flipkartPage = uClient.read() # reading the webpage
             uClient.close() # closing the connection to the web server
             flipkart_html = bs(flipkartPage, "html.parser") # parsing the webpage as HTML
-            bigboxes = flipkart_html.findAll("div", {"class": "bhgxx2 col-12-12"}) # seacrhing for appropriate tag to redirect to the product link
+            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"}) # seacrhing for appropriate tag to redirect to the product link
             del bigboxes[0:3] # the first 3 members of the list do not contain relevant information, hence deleting them.
             box = bigboxes[0] #  taking the first iteration (for demo)
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href'] # extracting the actual product link
             prodRes = requests.get(productLink) # getting the product page from server
+            prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser") # parsing the product page as HTML
-            commentboxes = prod_html.find_all('div', {'class': "_3nrCtb"}) # finding the HTML section containing the customer comments
+            print(prod_html)
+            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"}) # finding the HTML section containing the customer comments
 
             # table = db[searchString] # creating a collection with the same name as search string. Tables and Collections are analogous.
-            #filename = searchString+".csv" #  filename to save the details
-            #fw = open(filename, "w") # creating a local file to save the details
-            #headers = "Product, Customer Name, Rating, Heading, Comment \n" # providing the heading of the columns
-            #fw.write(headers) # writing first the headers to file
+            filename = searchString+".csv" #  filename to save the details
+            fw = open(filename, "w") # creating a local file to save the details
+            headers = "Product, Customer Name, Rating, Heading, Comment \n" # providing the heading of the columns
+            fw.write(headers) # writing first the headers to file
             reviews = [] # initializing an empty list for reviews
             #  iterating over the comment section to get the details of customer and their comments
             for commentbox in commentboxes:
                 try:
-                    name = commentbox.div.div.find_all('p', {'class': '_3LYOAd _3sxSiS'})[0].text
+                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
 
                 except:
                     name = 'No Name'
